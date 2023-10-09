26916b88cccefe1a5510c80f31ce79fc4a0be37f
Antonio Braga Dacome Neto <antoniodacome.sysmap@natura.net>
2023-10-09 13:15:12 -0300

    remove unused import

--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Animated, Easing, FlatList, I18nManager, Platform, ScrollView, View } from 'react-native';
-import PropTypes from 'prop-types';
 import shallowCompare from 'react-addons-shallow-compare';
 import {
     defaultScrollInterpolator,
